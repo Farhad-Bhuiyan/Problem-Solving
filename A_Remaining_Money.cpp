844535264f9f8a1b5054ccb6d4922ced45605546
Farhad-Bhuiyan <md1660782@gmail.com>
2026-05-12 21:25:23 +0600

    CodeChef Problem

---
+++ A_Remaining_Money.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,a,b;
+    cin >> n >> a >> b;
+    cout << n-(a*b) << endl;
+    return 0;
+}
