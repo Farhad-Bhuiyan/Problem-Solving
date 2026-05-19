b2710c600b2da1eb14223464c7667200998cb6b4
Farhad-Bhuiyan <md1660782@gmail.com>
2026-05-19 21:46:57 +0600

    CodeChef Prblem Difficulty:945

---
+++ D_Cab_Rides.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int to=0;
+        if(n<=2)cout << 200 << endl;
+        else
+        {
+            cout << n*100 << endl;
+        }
+    }
+    return 0;
+}
