58a609a6401a5e8f885c9757c0168a4467a995cb
Farhad-Bhuiyan <md1660782@gmail.com>
2026-06-02 22:15:53 +0600

    CodeChef Problem Difficulty:183

---
+++ A_Moneymaking.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,y;
+    cin >> x >> y;
+    cout << (long long)((x*5000) + (y*9800)) << endl;
+    return 0;
+}
