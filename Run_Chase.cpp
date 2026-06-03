770a11d274b2620e88dca024669351f546898bc6
Farhad-Bhuiyan <md1660782@gmail.com>
2026-06-03 22:38:36 +0600

    CodeChef STARTERS 241 ->A

---
+++ Run_Chase.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    cout << ceil((n+1)/20.0) << endl;
+    return 0;
+}
