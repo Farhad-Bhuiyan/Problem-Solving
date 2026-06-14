00f9709fea28a3e9428baab8c99faf7ccccc0a5c
Farhad-Bhuiyan <md1660782@gmail.com>
2026-06-14 23:36:26 +0600

    CodeChef STARTERS 235 ->A

---
+++ Contest_Registration_Fee.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,y;
+    cin >> x >> y;
+    if(y<=x)cout << 0 << endl;
+    else cout << 100 << endl;
+    return 0;
+}
