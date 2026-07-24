9f105fa910ccc2e4979fff902c6c70bafea6fc2f
Farhad-Bhuiyan <md1660782@gmail.com>
2026-07-24 12:59:56 +0600

    CodeChef STARTERS 233 - Div-4 ->C

--- K_Median.cpp
+++ K_Median.cpp
@@ -3,21 +3,39 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int t;
     cin >> t;
     while(t--)
     {
         int n,k;
         cin >> n >> k;
-        vector<int>v(n);
-        multset<int>se;
+        vector<int>a;
+        set<int>s;
         for(int i=0;i<n;i++)
         {
-            cin >> v[i];
+            int x;
+            cin >> x;
+            a.push_back(x);
         }
-        sort(v.begin(),v.end());
-        while
+        sort(a.begin(),a.end());
+        while(k--)
+        {
+            int id=ceil(a.size()/2.0);
+            s.insert(a[id-1]);
+            a.erase(find(a.begin(),a.end(),a[id-1]));
+        }
+        int id=ceil(a.size()/2.0);
+        s.insert(a[id-1]);
+        a.erase(find(a.begin(),a.end(),a[id-1]));
 
+        for(int i:s)
+        {
+            cout << i << " " ;
+        }
+        cout << endl;
     }
     return 0;
 }
