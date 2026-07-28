804d317b026664353b9f7db04a52f5c75c3d251e
Farhad-Bhuiyan <md1660782@gmail.com>
2026-07-28 19:55:25 +0600

    ITMO Academy: pilot course » Binary Search » Step 1 » Practice

---
+++ B_Closest_to_the_Left.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,k;
+    cin >> n >> k;
+    vector<long long>a(n);
+    for(long long &i:a)cin >> i;
+    while(k--)
+    {
+        long long x;
+        cin >> x;
+        bool fl=false;
+
+        int l=0,r=n-1,idx=0;
+        while(l<=r)
+        {
+            long long mid=(l+r)/2;
+            if(a[mid]<=x)
+            {
+                idx=mid+1;
+                l=mid+1;
+            }
+            if(a[mid]>x)
+            {
+                r=mid-1;
+            }
+        }
+        cout << idx << endl;
+    }
+    return 0;
+}
