9fcf4879c2a2b7336036b716584f2cb752fe6b94
Farhad-Bhuiyan <md1660782@gmail.com>
2026-09-02 21:27:52 +0600

    CodeChef STARTERS 254-Division-4->B

---
+++ Reach_Weight.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n/2)*30 + (n%2)*20 << endl;
+    }
+    return 0;
+}
